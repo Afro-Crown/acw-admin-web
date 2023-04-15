132774c3d022395fa6ef688649b8f333e1da8390
GuilhermeArcanjo300 <guilherme.arcanjo300@gmail.com>
2023-04-15 10:56:48 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Julius_Sans_One } from 'next/font/google'
+
+const julius = Julius_Sans_One({
+  subsets: ['latin'],
+  weight: '400'
+})
+
+export default function Home() {
+  return (
+    <main className={`flex min-h-screen flex-col items-center justify-center ${julius.className}`}>
+      <label className={`text-9xl `}>AFRO CROWN</label>
+    </main>
+  )
+}
