de7f592d9c6e4f2d6a6be87dff1438aaead207ab
Claudio Santos <claudiogsssantos@gmail.com>
2023-11-07 00:41:12 -0300

    feat: creating footer component

--- src/app/home/components/footer.tsx
+++ src/app/home/components/footer.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 import footerLogo from "../../../../public/footer-logo.svg";
 import linkedinLogo from "../../../../public/linkedin-logo.svg";
 import instagramLogo from "../../../../public/instagram-logo.svg";
 import footerDivider from "../../../../public/divider.svg";
-import { CaretRight } from "@phosphor-icons/react";
+import downloadApp from "../../../../public/download-app-btn.svg";
+import googlePlay from "../../../../public/googleplay-btn.svg";
+import appleStore from "../../../../public/applestore.svg";
+import { AppStoreLogo, CaretRight } from "@phosphor-icons/react";
 
 const Footer = () => {
   return (
-    <footer className="h-[287px] w-full bg-[#A21A1A]">
-      <nav className=" h-[80%] pt-8">
+    <footer className="h-[287px] w-full bg-[#A21A1A] flex flex-col items-center gap-1">
+      <nav className=" h-[80%] pt-8 w-[60%] flex flex-row justify-between">
         <div className="flex flex-col justify-between gap-12 pt-8">
           <div className="">
             <Image alt="AfroCrown logo" src={footerLogo}/>
@@ -20,17 +24,19 @@ const Footer = () => {
           </div>
         </div>
 
-        <div>
-
+        <div className="flex flex-col h-full justify-around">
+          <Image alt="download app" src={downloadApp} />
+          <Image alt="download from google play" src={googlePlay} />
+          <Image alt="download from apple store" src={appleStore} />
         </div>
       </nav>
-      <Image alt="divider" src={footerDivider} />
-      <footer className=" h-[20%] flex justify-between items-center">
+      <div className="w-[60%] border border-b-0 border-[#FFEAD4AA]"></div>
+      <footer className="w-[60%] h-[20%] flex justify-between items-center">
         <div className="w-[50%] flex justify-start pl-2 font-bold">
           <h4>SEJA UM VOLUNTÁRIO</h4>
-          <CaretRight />
+          
         </div>
-        <div className="w-[50%] flex justify-around">
+        <div className="w-[50%] flex justify-end gap-4">
           <span className="text-sm">Termos de uso</span>
           <span className="text-sm">Políticas de privacidade e dados</span>
         </div>
