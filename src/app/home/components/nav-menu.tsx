b9ac663860f624649640d58521ba6510d57ac238
Claudio Santos <claudiogsssantos@gmail.com>
2023-11-16 19:09:19 -0300

    fix: hover navmenu

--- src/app/home/components/nav-menu.tsx
+++ src/app/home/components/nav-menu.tsx
@@ -7,28 +7,28 @@ const NavMenu = () => {
   return (
     <div className="rounded-md p-3 bg-white w-[279px] h-[344px] text-gray-600 absolute top-[100px] left-[1100px]">
       <h2 className="w-full flex items-start text-xl pl-2 py-4">Olá, Dellas &...</h2>
-      <button className="flex items-center w-full p-4 justify-between">
+      <button className="flex items-center w-full p-4 justify-between hover:bg-gray-50">
         <div className="flex items-center gap-2">
           <Image alt="user icon" src={userIconMenu} />
           <span>Seu perfil</span>
         </div>
         <CaretRight />
       </button>
-      <button className="flex items-center w-full p-4 justify-between">
+      <button className="flex items-center w-full p-4 justify-between hover:bg-gray-50">
         <div className="flex items-center gap-2">
         <Image alt="user icon" src={scheduleIconMenu} />
           <span>Agendamentos</span>
         </div>
         <CaretRight />
       </button>
-      <button className="flex items-center w-full p-4 justify-between">
+      <button className="flex items-center w-full p-4 justify-between hover:bg-gray-50">
         <div className="flex items-center gap-2">
           <Question size={24} color="#787878"/>
           <span>Ajuda</span>
         </div>
         <CaretRight />
       </button>
-      <button className="flex items-center w-full p-4 justify-between">
+      <button className="flex items-center w-full p-4 justify-between hover:bg-gray-50">
         <div className="flex items-center gap-2">
           <Gear size={24} color="#787878"/>
           <span>Configurações</span>
@@ -36,7 +36,7 @@ const NavMenu = () => {
         <CaretRight />
       </button>
       <div className="w-full flex items-start pl-2">
-        <button className="rounded-md bg-[#A21A1A] text-white p-3 mt-4 font-semibold">
+        <button className="rounded-md bg-[#A21A1A] text-white p-3 mt-4 font-semibold hover:bg-[#A21A1AAA]">
           Sair da conta
         </button>
       </div>
