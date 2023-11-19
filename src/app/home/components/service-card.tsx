7676fa62c42d9ffcb7bd8e1988aafab8819272d8
Claudio Santos <claudiogsssantos@gmail.com>
2023-11-19 19:54:28 -0300

    fix: responsividade

--- src/app/home/components/service-card.tsx
+++ src/app/home/components/service-card.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const ServiceCard = () => {
   return ( 
-    <section className="flex flex-row justify-between px-1 md:p-2 lg:p-2 w-screen md:w-[700px] lg:w-[984px]max-w-[984px]">
+    <section className="flex flex-row justify-between px-1 md:p-2 lg:p-2 w-screen md:w-[700px] lg:w-[985px] max-w-[984px]">
       <div className="flex flex-row h-[129px] gap-4">
         <div className="flex flex-col items-center justify-around w-10% shadow-md rounded px-4 py-2">
           <span className="text-3xl">19</span>
