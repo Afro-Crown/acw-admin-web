941c4b2e3f4f96acd31db18def7d1a3968068259
Claudio Santos <claudiogsssantos@gmail.com>
2023-11-24 20:31:15 -0300

    feat: criando modal de editar serviço

--- src/app/services/components/edit-service-modal.tsx
+++ src/app/services/components/edit-service-modal.tsx
@@ -1,6 +1,7 @@
 import { X } from "@phosphor-icons/react";
 import ServiceTags from "./service-tags";
 import ServiceInfos from "./service-infos";
+import ServiceSchedule from "./service-schedule";
 
 interface ModalAcceptProps {
     closeModal: () => void;
@@ -9,7 +10,7 @@ interface ModalAcceptProps {
 const EditServiceModal = ({closeModal}: ModalAcceptProps) => {
   return ( 
     <div className="fixed w-screen h-screen left-0 top-0 flex justify-center items-center bg-white bg-opacity-10">
-            <div className="w-[70%] p-4 rounded-md bg-white font-ASSISTANT shadow-md">
+            <div className="w-full md:w-[70%] lg:w-[70%] p-4 rounded-md bg-white font-ASSISTANT shadow-md">
                 <div className="flex justify-start mb-2">
                     <X size={24} onClick={closeModal} className="cursor-pointer"/>
                 </div>
@@ -31,7 +32,7 @@ const EditServiceModal = ({closeModal}: ModalAcceptProps) => {
                     </div>
                   </div>
                   <div className="w-1/2 border border-yellow-500">
-                    
+                    <ServiceSchedule />
                   </div>
                 </main>
             </div>
--- src/app/services/components/service-infos.tsx
+++ src/app/services/components/service-infos.tsx
@@ -1,8 +1,30 @@
 const ServiceInfos = () => {
   return ( 
-    <div>
-      <h2 className="text-lg font-semibold">Nome</h2>
+    <div className="pb-20">
+      <div>
+        <h2 className="text-lg font-semibold">Nome</h2>
+        <input type="text" placeholder="Ex:Hidratação + Pontas" className="w-full appearance-none dark:focus:border-gray-500 focus:outline-none focus:ring-0 focus:text-black focus:border-orange-500 border-0 border-b-2 h-10 bg-transparent"></input>
+        <span className="text-sm text-[#949494]">máximo 20 caracteres</span>
+      </div>
+      <div>
+        <h2 className="text-lg font-semibold">Preço</h2>
+        <input type="text" placeholder="R$" className="w-full appearance-none dark:focus:border-gray-500 focus:outline-none focus:ring-0 focus:text-black focus:border-orange-500 border-0 border-b-2 h-10 bg-transparent"></input>
+      </div>
+      <div className="w-[30%]">
+        <h2 className="text-lg font-semibold">Duração</h2>
+        <div className="flex items-center text-center text-md text-[#949494] gap-0">
+          <input type="text" placeholder="   00" className="w-8 appearance-none dark:focus:border-gray-500 focus:outline-none focus:ring-0 focus:text-black focus:border-orange-500 border-0 border-b-2 h-10 bg-transparent"></input>
+          <span>h</span>
+          <input type="text" placeholder="   00" className="w-8 appearance-none dark:focus:border-gray-500 focus:outline-none focus:ring-0 focus:text-black focus:border-orange-500 border-0 border-b-2 h-10 bg-transparent"></input>
+          <span>min</span>
+        </div>
+      </div>
+      <div>
+        <h2 className="text-lg font-semibold">Descrição</h2>
+        <input type="text" placeholder="(Opcional)" className="w-full appearance-none dark:focus:border-gray-500 focus:outline-none focus:ring-0 focus:text-black focus:border-orange-500 rounded-xl border-2 bg-transparent p-4 h-32"></input>
+      </div>
     </div>
+    
    );
 }
  
