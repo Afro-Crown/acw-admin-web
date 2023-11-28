4cb24ea59798edba6890c93310c7534bfea6f7fb
Claudio Santos <claudiogsssantos@gmail.com>
2023-11-27 21:45:42 -0300

    feat: animação de clique dos botões

--- src/app/services/components/hours-cards.tsx
+++ src/app/services/components/hours-cards.tsx
@@ -2,23 +2,30 @@
 import { useState } from "react";
 
 const HourCards = () => {
+
+  const [changeColor, setChangeColor] = useState(false);
+
+  function handleClick () {
+    setChangeColor(!changeColor)
+  }
+
   return ( 
     <div className="flex-wrap p-2">
-      <button className=" m-[2px] rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center">7:00</button>
-      <button className=" m-[2px] rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center">8:00</button>
-      <button className=" m-[2px] rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center">9:00</button>
-      <button className=" m-[2px] rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center">10:00</button>
-      <button className=" m-[2px] rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center">11:00</button>
-      <button className=" m-[2px] rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center">12:00</button>
-      <button className=" m-[2px] rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center">13:00</button>
-      <button className=" m-[2px] rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center">14:00</button>
-      <button className=" m-[2px] rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center">15:00</button>
-      <button className=" m-[2px] rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center">16:00</button>
-      <button className=" m-[2px] rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center">18:00</button>
-      <button className=" m-[2px] rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center">19:00</button>
-      <button className=" m-[2px] rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center">20:00</button>
-      <button className=" m-[2px] rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center">21:00</button>
-      <button className=" m-[2px] rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center">22:00</button>
+      <button onClick={handleClick} className={`m-[2px] rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>7:00</button>
+      <button onClick={handleClick} className={`m-[2px] rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>8:00</button>
+      <button onClick={handleClick} className={`m-[2px] rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>9:00</button>
+      <button onClick={handleClick} className={`m-[2px] rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>10:00</button>
+      <button onClick={handleClick} className={`m-[2px] rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>11:00</button>
+      <button onClick={handleClick} className={`m-[2px] rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>12:00</button>
+      <button onClick={handleClick} className={`m-[2px] rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>13:00</button>
+      <button onClick={handleClick} className={`m-[2px] rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>14:00</button>
+      <button onClick={handleClick} className={`m-[2px] rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>15:00</button>
+      <button onClick={handleClick} className={`m-[2px] rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>16:00</button>
+      <button onClick={handleClick} className={`m-[2px] rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>18:00</button>
+      <button onClick={handleClick} className={`m-[2px] rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>19:00</button>
+      <button onClick={handleClick} className={`m-[2px] rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>20:00</button>
+      <button onClick={handleClick} className={`m-[2px] rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>21:00</button>
+      <button onClick={handleClick} className={`m-[2px] rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-1 w-14 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>22:00</button>
     </div>
    );
 }
--- src/app/services/components/week-days-cards.tsx
+++ src/app/services/components/week-days-cards.tsx
@@ -2,16 +2,21 @@
 import { useState } from "react";
 
 const WeekDayCards = () => {
+  const [changeColor, setChangeColor] = useState(false);
+
+  function handleClick () {
+    setChangeColor(!changeColor)
+  }
 
   return ( 
     <div className="flex gap-2">
-      <button className="rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-2 w-20 text-center">Seg</button>
-      <button className="rounded-lg bg-[#FFD6AD] bg-opacity-20 text-sm text-[#FF6734] py-1 px-2 w-20 text-center">Ter</button>
-      <button className="rounded-lg bg-[#FFD6AD] bg-opacity-20 text-sm text-[#FF6734] py-1 px-2 w-20 text-center">Qua</button>
-      <button className="rounded-lg bg-[#FFD6AD] bg-opacity-20 text-sm text-[#FF6734] py-1 px-2 w-20 text-center">Qui</button>
-      <button className="rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-2 w-20 text-center">Sex</button>
-      <button className="rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-2 w-20 text-center">Sab</button>
-      <button className="rounded-lg bg-[#C7C7C7] bg-opacity-20 text-sm text-[#616161] py-1 px-2 w-20 text-center">Dom</button>
+      <button onClick={handleClick} className={`rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-2 w-20 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>Seg</button>
+      <button onClick={handleClick} className={`rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-2 w-20 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>Ter</button>
+      <button onClick={handleClick} className={`rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-2 w-20 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>Qua</button>
+      <button onClick={handleClick} className={`rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-2 w-20 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>Qui</button>
+      <button onClick={handleClick} className={`rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-2 w-20 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>Sex</button>
+      <button onClick={handleClick} className={`rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-2 w-20 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>Sab</button>
+      <button onClick={handleClick} className={`rounded-lg bg-opacity-20 text-sm text-[#616161] py-1 px-2 w-20 text-center ${(changeColor === true) ? 'bg-[#FFD6AD] text-[#FF6734]': 'bg-[#c7c7c7]' }`}>Dom</button>
     </div>
    );
 }
