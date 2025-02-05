23f2acc6dafa2de67b60786ca3edd57108031705
Derick Martins <derickvice21@gmail.com>
2025-02-04 23:13:43 -0300

    feat/home
    Criação do header, footer e da página Home.

--- src/components/molecules/header.tsx
+++ src/components/molecules/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center bg-[#FFEAD4] w-full h-20 px-64 fixed top-0">      
       <Image src={logo} alt="Logo" />
-      <span className="bg-secondary px-40 py- mx-3 rounded-sm"></span>
+      <span className="bg-secondary px-40 py-[1px] mx-3 rounded-sm"></span>
       <div className="flex items-center mr-4">
         <Link href="/volunteer" className="pr-10 py-2 border-r-2 border-secondary  text-secondary">Seja um voluntário</Link>
         <Link href="/about" className="px-10 py-2 border-r-2 border-secondary  text-secondary">Sobre</Link>
