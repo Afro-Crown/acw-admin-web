4bd572af59c484048bd2e82761f7da0c80a230ce
DavidMartins27 <135028080+DavidMartins27@users.noreply.github.com>
2025-03-24 21:54:13 -0300

    feat/shopEntity
    Criação da entidade do shop

---
+++ src/common/entities/shop.ts
@@ -0,0 +1,26 @@
+export interface commentsEntity {
+    id: string;
+    name: string;
+    email: string;
+    address: string;
+    neighboard: string;
+    complement: string;  
+    number: string;
+    city: string;
+    cep: string;
+    state: string;
+    cnpj: string;
+    phone: string;
+    owner_name: string;
+    image: string;
+    banner: string;
+    isOpen: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+    isActive: boolean;
+    comments: string[];
+    staff: string[];
+    services: string[];
+    inAnalising: boolean;
+    schedules: string[];
+}
