50eddf93c123b50f2611c609459681b92fedde99
DavidMartins27 <135028080+DavidMartins27@users.noreply.github.com>
2025-03-24 21:48:31 -0300

    feat/commentsEntity
    Criação da entidade de comments

---
+++ src/common/entities/comments.ts
@@ -0,0 +1,9 @@
+export interface commentsEntity {
+    id: string;
+    rating: string;
+    description: string;
+    user_id: string;
+    shop_id: string;
+    tags: string;
+    created_at: Date;
+}
