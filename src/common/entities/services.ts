7d0ea960d6eca1f95d9b83e394695b1c7f791b05
Derick Martins <derickvice21@gmail.com>
2025-03-24 21:42:37 -0300

    feat/servicesEntity
    Criação da entidade services

---
+++ src/common/entities/services.ts
@@ -0,0 +1,12 @@
+export interface servicesEntity {
+  id: string;
+  type: string;
+  price: string;
+  name: string;
+  duration: string;
+  description: string;
+  image: string;
+  staff: string;
+  created_at : Date;
+  updated_at : Date;
+}
