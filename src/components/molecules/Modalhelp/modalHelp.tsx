db7e82246ed59a4185b9b95228ca7ca31ebedc84
DavidMartins27 <135028080+DavidMartins27@users.noreply.github.com>
2025-03-31 04:52:51 -0300

    fix/modalHelp
    Correção do modalHelp

--- src/components/molecules/Modalhelp/modalHelp.tsx
+++ src/components/molecules/Modalhelp/modalHelp.tsx
@@ -9,7 +9,7 @@ import {
 import { ModalProps } from "./types";
 import Image from "next/image";
 import { useState } from "react";
-import imagemDownlaod from "../../../../public/download-app-btn.svg";
+import imagemDownlaod from "../../../../public/garfo.svg";
 import { Check } from "lucide-react";
 
 export function ModalHelp({ isOpen, setIsOpen }: ModalProps) {
@@ -20,19 +20,28 @@ export function ModalHelp({ isOpen, setIsOpen }: ModalProps) {
   return (
     <>
       <Dialog open={isOpen} onOpenChange={setIsOpen}>
-        <DialogContent className="absolute left-[100rem] top-[43rem] sm:max-w-[35rem] sm:max-h-[40rem]">
-          <DialogHeader className="gap-4">
+        <DialogContent buttonLeft className="sm:rounded-none left-auto right-[-18rem] top-[35rem] sm:max-w-[35rem] sm:max-h-[40rem] min-h-[104%]">
+          <DialogHeader className="flex flex-col max-h-[1px] gap-4 mt-6">
             <p className="text-sm font-light opacity-60">Dúvidas frequentes</p>
             <DialogTitle className="text-2xl">Como cadastrar um salão?</DialogTitle>
           </DialogHeader>
-          <div className="grid gap-4">
-            <p className="text-sm font-light">Para isso é só baixar a versão abaixo e cadastrar o seu salão. Entraremos em contato para garantir que está tudo certo e em poucos dias seu salão aparecerá para clientes próximos.</p>
-            <p className="text-sm font-light">A partir do <b>aplicativo para salões Afro Crown</b>, você receberá e-mails de quando um cliente <b>agendar um horário.</b></p>
-            <Image src={imagemDownlaod} alt="Imagem de exemplo" />
+          <div className="grid">
+            <p className="text-sm font-light ">Para isso é só baixar a versão abaixo e cadastrar o seu salão. Entraremos em contato para garantir que está tudo certo e em poucos dias seu salão aparecerá para clientes próximos.</p>
+            <p className="text-sm font-light ">A partir do <b>aplicativo para salões Afro Crown</b>, você receberá e-mails de quando um cliente <b>agendar um horário.</b></p>
+            <div className="flex gap-6">
+              <div className="flex items-center justify-center w-[58px] h-[56px] bg-[#A21A1A] rounded-2xl">
+                <Image src={imagemDownlaod} alt="Imagem de exemplo" />
+              </div>
+              <div>
+                <p className="text-sm text-[#616161]">Afro Crown ADM</p>
+                <p className="text-xs font-light text-[#616161]">versão 1.0</p>
+                <p className="text-sm text-[#FF6734]">Baixar app</p>
+              </div>
+            </div>
           </div>
           <div className="flex flex-col gap-4">
             <h2 className="text-2xl font-light">Não encontrou sua dúvida?</h2>
-            <div className="flex gap-5">
+            <div className="flex gap-5 mb-[20rem]">
               <button
                 className="w-[9rem] h-[3rem] border border-[#FF6734] rounded-3xl text-[#FF6734]"
                 onClick={() => {
